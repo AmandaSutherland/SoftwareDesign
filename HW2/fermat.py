b1bfcc6bf363fc642325f188961aa13ed9c683cd
AmandaSutherland <amanda.h.sutherland.93@gmail.com>
2014-02-03 00:43:44 -0500

    committing Fermat

--- HW2/fermat.py
+++ HW2/fermat.py
@@ -1,7 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Feb  2 13:36:23 2014
+Spyder Editor
 
-@author: asutherland
+This temporary script file is located here:
+/home/asutherland/.spyder2/.temp.py
 """
 
+## Fermat's Last Theorem 
+
+def check_fermat(a,b,c,n):
+    
+    if a**(n)+b**(n)==c**(n) and n>2:
+        return "Holy smokes, Fermat was wrong!"
+    else:
+        return "No, that doesn't work" 
+    
+if __name__=='__main__':
+    a = 1
+    b = 2
+    c = 3
+    n = 3
+    print check_fermat(a,b,c,n)
